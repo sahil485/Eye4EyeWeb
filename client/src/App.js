1e880ad142315989a5b367e9bc1d8b20b958cf3a
unknown <sahilchatiwala@gmail.com>
2021-10-26 14:17:43 -0400

    app context to prevent infinite renders

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import GlobalStyle from './globalStyles';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { Navbar, Footer } from './components';
@@ -7,24 +7,34 @@ import Home from './pages/HomePage/Home';
 import Hoodies from './pages/Hoodies/Hoodies';
 import Tshirts from './pages/T-Shirts/Tshirts';
 import Signup from './pages/Sign Up/Signup';
+import { AppContext } from './Context';
+import axios from 'axios';
 
 function App() {
+
+  const [ email , setEmail ] = useState("");
+  const dispatchEmail = (email) => {
+    axios.post('/emails', email)
+  }
+
   return (
     //provider gives a way to pass data to other sections without having to pass the props manually through the component tree
     //especially useful for applications where info has to be customized for users
       <div className="App">
-        <Router>
-          <GlobalStyle />
-          <ScrollToTop />
-          <Navbar />
-          <Switch>
-            <Route path='/' exact component={Home}/>
-            <Route path='/Hoodies' exact component={Hoodies}/>
-            <Route path='/Tshirts' exact component={Tshirts}/>
-            <Route path='/Signup' exact component={Signup}/>
-          </Switch>
-          <Footer />
-        </Router>
+        <AppContext.Provider>
+          <Router>
+            <GlobalStyle />
+            <ScrollToTop />
+            <Navbar />
+            <Switch>
+              <Route path='/' exact component={Home}/>
+              <Route path='/Hoodies' exact component={Hoodies}/>
+              <Route path='/Tshirts' exact component={Tshirts}/>
+              <Route path='/Signup' exact component={Signup}/>
+            </Switch>
+            <Footer />
+          </Router>
+        </AppContext.Provider>
       </div>
   );
 }
