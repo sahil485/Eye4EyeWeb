c6d18efd9abc82f6acce323ccd58b60e35f7d519
sillyhill-c <sahil.chatiwala@gmail.com>
2021-10-28 22:29:35 -0400

    added req functionality

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ app.use(express.json())
 app.use(cors())
 
 
-const uri = "mongodb+srv://sillyhill:<password>@eye4eye.pt0wj.mongodb.net/emailList?retryWrites=true&w=majority"
+const uri = "mongodb+srv://sillyhill:pgxtpp5k@eye4eye.pt0wj.mongodb.net/emailList?retryWrites=true&w=majority"
 
 mongoose.connect(uri, ({useNewUrlParser:true})).then(console.log("connected to MongoDB")).catch(err => console.log(err))
 
@@ -19,15 +19,19 @@ const emailSchema = new mongoose.Schema({
 const EmailModel = mongoose.model('EmailList', emailSchema)
 
 app.get('/emails', (req, res) =>{
-    EmailModel.find().then(info => console.log(info.json()))
+    EmailModel.find().then(info => console.log(info)).catch(err => console.log(err))
 })
+
 
 app.post('/emails', (req, res) => {
     const newEmailModel = new EmailModel({
         email: req.body.email,
     })
-    newEmailModel.save().then(res => console.log(res))
+    newEmailModel.save().then(res => console.log(res)).catch(err => console.log(err))
 })
 
+app.delete('/emails/:id', (req, res) => {
+    EmailModel.findByIdAndDelete(req.params.id).then(info => console.log(info)).catch(err => console.log(err))
+})
 
 app.listen(5000, ()=>{console.log("Server is running at port 5000")})
