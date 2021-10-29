24a203acf0a43d67baca8f19f075c74453183af4
sillyhill-c <sahil.chatiwala@gmail.com>
2021-10-28 23:14:46 -0400

    edited email subscription functionality so that it only adds new emails, instead of repeatedly adding already-present emails to the subscription-list collection

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ app.use(express.json())
 app.use(cors())
 
 
-const uri = "mongodb+srv://sillyhill:pgxtpp5k@eye4eye.pt0wj.mongodb.net/emailList?retryWrites=true&w=majority"
+const uri = "mongodb+srv://sillyhill:<password>@eye4eye.pt0wj.mongodb.net/emailList?retryWrites=true&w=majority"
 
 mongoose.connect(uri, ({useNewUrlParser:true})).then(console.log("connected to MongoDB")).catch(err => console.log(err))
 
@@ -16,7 +16,7 @@ const emailSchema = new mongoose.Schema({
     email: String
 })
 
-const EmailModel = mongoose.model('EmailList', emailSchema)
+const EmailModel = mongoose.model('EmailSubscriptionList', emailSchema)
 
 app.get('/emails', (req, res) =>{
     EmailModel.find().then(info => console.log(info)).catch(err => console.log(err))
@@ -24,14 +24,25 @@ app.get('/emails', (req, res) =>{
 
 
 app.post('/emails', (req, res) => {
-    const newEmailModel = new EmailModel({
-        email: req.body.email,
+    EmailModel.find({email: req.body.email}, {_id: 1, email: 1}).then(res => {
+        if(res.length<1)
+        {
+            console.log("new email")
+            const newEmailModel = new EmailModel({
+                email: req.body.email,
+            })
+            newEmailModel.save().catch(err => console.log(err)).then(res => console.log(res)).then(console.log("success"))
+        }
+        else
+        {
+            console.log(`Email: ${req.body.email} alr exists ${res.length} times in database`)
+        }
+
     })
-    newEmailModel.save().then(res => console.log(res)).catch(err => console.log(err))
 })
 
 app.delete('/emails/:id', (req, res) => {
-    EmailModel.findByIdAndDelete(req.params.id).then(info => console.log(info)).catch(err => console.log(err))
+    EmailModel.findByIdAndDelete(req.params.id).then(res => console.log(`successfully deleted entry with ID: ${req.params.id}`)).catch(err => console.log(err))
 })
 
 app.listen(5000, ()=>{console.log("Server is running at port 5000")})
