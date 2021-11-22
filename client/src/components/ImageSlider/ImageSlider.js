37e859dc415d0fc60b80ae495a300cd533a221e6
sillyhill-c <sahil.chatiwala@gmail.com>
2021-11-21 21:35:47 -0500

    failed slider work

--- client/src/components/ImageSlider/ImageSlider.js
+++ client/src/components/ImageSlider/ImageSlider.js
@@ -1,23 +1,25 @@
 import React from 'react'
-import Slider from 'react-animated-slider';
+import Slider from 'react-animated-slider'
 import 'react-animated-slider/build/horizontal.css';
+import tshirt from '../ImageSlider/Png/tshirt.jpg'
+import f1 from '../ImageSlider/Png/Founder1.jpg'
+import f2 from '../ImageSlider/Png/Founder2.PNG'
+import p from '../ImageSlider/Png/placeholder.jpg'
+import f13 from '../ImageSlider/Png/Sweatshirts/1OF3Front.png'
+import b13 from '../ImageSlider/Png/Sweatshirts/1OF3Back.png'
+import f23 from '../ImageSlider/Png/Sweatshirts/2OF3Front.png'
+import b23 from '../ImageSlider/Png/Sweatshirts/2OF3Back.png'
 
 const ImageSlider = () =>
 {
 
-    const slides = [
-        { title: 'First item', description: 'Lorem ipsum', image: "../tshirt.jpg"},
-        { title: 'Second item', description: 'Lorem ipsum', image: "../tshirt.jpg"}
-      ]; 
-
-    return( 
-        <Slider>
-        {slides.map((slide, index) => <div key={index}>
-            <h2>{slide.title}</h2>
-            <div>{slide.description}</div>
-            <img src = {require(slide.image).default}/>
-            </div>)}
-        </Slider>
+    const slides = [tshirt, f1, f2, p, f13, b13, f23, b23]
+    return(
+            <Slider>
+            {slides.map((slide, index) => <div key={index}>
+                <img src = {slide}/>
+                </div>)}
+            </Slider>
     );
 }
 
