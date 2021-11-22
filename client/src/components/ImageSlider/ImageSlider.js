8f21c91c3b472831961f0c99ec226a0b632be0a3
sillyhill-c <sahil.chatiwala@gmail.com>
2021-11-21 21:57:51 -0500

    somewhat working slider

--- client/src/components/ImageSlider/ImageSlider.js
+++ client/src/components/ImageSlider/ImageSlider.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import Slider from 'react-animated-slider'
-import 'react-animated-slider/build/horizontal.css';
+import slider from 'react-animated-slider/build/horizontal.css';
 import tshirt from '../ImageSlider/Png/tshirt.jpg'
 import f1 from '../ImageSlider/Png/Founder1.jpg'
 import f2 from '../ImageSlider/Png/Founder2.PNG'
@@ -15,10 +15,14 @@ const ImageSlider = () =>
 
     const slides = [tshirt, f1, f2, p, f13, b13, f23, b23]
     return(
-            <Slider>
-            {slides.map((slide, index) => <div key={index}>
-                <img src = {slide}/>
-                </div>)}
+
+            <Slider classNames={slider}>
+            {slides.map(item => (
+                <div
+                    style={{ background: `url('https://helpx.adobe.com/content/dam/help/en/photoshop/using/convert-color-image-black-white/jcr_content/main-pars/before_and_after/image-before/Landscape-Color.jpg') no-repeat center center` }}
+                >   
+                </div>
+            ))}
             </Slider>
     );
 }
